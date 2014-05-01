89386e0d8c8562f8b3483ed607ec0bf1c086a8b9
Atom Powers <atom.powers@seattlebiomed.org>
2014-05-01 13:36:21 -0700

    fix quote bug

--- lib/facter/pandorafms_version.rb
+++ lib/facter/pandorafms_version.rb
@@ -2,7 +2,7 @@
 Facter.add(:pandorafms_version) do
   case Facter.value(:osfamily)
   when 'Debian'
-    setcode '/usr/bin/dpkg -l pandora* 2>/dev/null| /usr/bin/awk \'{if ($1 == "ii"){print $3}}\''
+    setcode '/usr/bin/dpkg -l \'pandora*\' 2>/dev/null| /usr/bin/awk \'{if ($1 == "ii"){print $3}}\''
   else
     setcode puts 'unknown'
   end
